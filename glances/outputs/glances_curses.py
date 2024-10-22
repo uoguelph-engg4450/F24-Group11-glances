44157beb2517bb23f635d5d04371c8d59525faf4
mmacdo55 <mmacdo55@uoguelph.ca>
2024-10-22 17:32:56 -0400

    saaaaaaaaaaaaaaaa

--- glances/outputs/glances_curses.py
+++ glances/outputs/glances_curses.py
@@ -131,14 +131,14 @@ class _GlancesCurses:
     def _handle_backtick(self):
         #self.colors_list = self.switcher.switchLDmode()
         if self.light_mode:
+            self.colors_list = GlancesColors(self.args, True).get()
             curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_WHITE)
             self.screen.bkgdset(' ', curses.color_pair(1) | curses.A_REVERSE)  # Set the new background color
-            self.colors_list = GlancesColors(self.args, True).get()
             self.light_mode = False
         else:
+            self.colors_list = GlancesColors(self.args, False).get()
             curses.init_pair(1, -1, -1)
             self.screen.bkgdset(' ', curses.color_pair(1))  # Set the new background color
-            self.colors_list = GlancesColors(self.args, False).get()
             self.light_mode = True
         self.screen.refresh()
 
