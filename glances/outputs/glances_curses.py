b49578eff65d35654c90ef267d9b87ebda6c80d9
mmacdo55 <mmacdo55@uoguelph.ca>
2024-10-23 19:31:35 -0400

    closed and reopened client

--- glances/outputs/glances_curses.py
+++ glances/outputs/glances_curses.py
@@ -15,7 +15,7 @@ from glances.events_list import glances_events
 from glances.globals import MACOS, WINDOWS, disable, enable, itervalues, nativestr, u
 from glances.logger import logger
 from glances.outputs.glances_colors import GlancesColors
-from glances.outputs.glances_curses_browser import GlancesCursesBrowser
+from glances.client_browser import GlancesClientBrowser
 from glances.outputs.glances_unicode import unicode_message
 from glances.processes import glances_processes, sort_processes_key_list
 from glances.timer import Timer
@@ -137,8 +137,9 @@ class _GlancesCurses:
             self.light_mode = True
         self.edit_filter = True
         self.end()
-        self.__init__(self.config, self.args)
-        GlancesCursesBrowser(self)
+        GlancesClientBrowser()
+        #self.__init__(self.config, self.args)
+        #GlancesCursesBrowser(self)
 
     def __init__(self, config=None, args=None):
         # Init
