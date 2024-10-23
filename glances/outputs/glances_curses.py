dfd733b85cc86482fc802f02178e259e75f8652f
mmacdo55 <mmacdo55@uoguelph.ca>
2024-10-23 19:11:28 -0400

    push

--- glances/outputs/glances_curses.py
+++ glances/outputs/glances_curses.py
@@ -137,7 +137,6 @@ class _GlancesCurses:
         self.edit_filter = True
         self.end()
         self.__init__(self.config, self.args)
-        self.update()
 
     def __init__(self, config=None, args=None):
         # Init
