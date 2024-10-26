0cc45f3ea1daddd0fe302a9b134ef32f01b8d8a5
ZachMcMurray <zmcmurray14@gmail.com>
2024-10-26 11:20:48 -0400

    byte threshold add worked on by zach nicky sheel

--- glances/plugins/plugin/model.py
+++ glances/plugins/plugin/model.py
@@ -455,16 +455,15 @@ class GlancesPluginModel:
                         'optional': False,
                         'additional': False,
                         'splittable': False,
-                        'hidden': True,
                     }
                     # Manage the hidden feature
                     # Allow to automatically hide fields when values is never different than 0
                     # Refactoring done for #2929
                     if not self.hide_zero:
-                        value['hidden'] = True
+                        value['hidden'] = False
                     elif key in self.views and field in self.views[key] and 'hidden' in self.views[key][field]:
                         value['hidden'] = self.views[key][field]['hidden']
-                        if field in self.hide_zero_fields and i[field] >= 1000000000000000000000000000000000000000:
+                        if field in self.hide_zero_fields and i[field] > 14000:
                             value['hidden'] = False
                     else:
                         value['hidden'] = field in self.hide_zero_fields
@@ -477,16 +476,16 @@ class GlancesPluginModel:
                     'optional': False,
                     'additional': False,
                     'splittable': False,
-                    'hidden': True,
+                    'hidden': False,
                 }
                 # Manage the hidden feature
                 # Allow to automatically hide fields when values is never different than 0
                 # Refactoring done for #2929
                 if not self.hide_zero:
-                    value['hidden'] = True
+                    value['hidden'] = False
                 elif field in self.views and 'hidden' in self.views[field]:
                     value['hidden'] = self.views[field]['hidden']
-                    if field in self.hide_zero_fields and self.get_raw()[field] >= 10000000000000000000000000000000000000000000000000000000:
+                    if field in self.hide_zero_fields and self.get_raw()[field] > 14000:
                         value['hidden'] = False
                 else:
                     value['hidden'] = field in self.hide_zero_fields
